6e1bbcc626eaaed3546f3357177dc59ead394213
jingle-jew <jmb.whp@gmail.com>
2026-06-11 21:41:47 -0400

    Fix path bugs

--- custom_components/mha_widget_hub/const.py
+++ custom_components/mha_widget_hub/const.py
@@ -1,7 +1,7 @@
 """Constants for MHA Widget Hub."""
 
 DOMAIN = "mha_widget_hub"
-VERSION = "0.1.0"
+VERSION = "0.1.1"
 
 PANEL_COMPONENT = "mha-control-hub"
 PANEL_URL_PATH = "mha-control-hub"
@@ -10,4 +10,3 @@ PANEL_ICON = "mdi:view-dashboard"
 
 STATIC_URL_PATH = "/mha_widget_hub_static"
 FRONTEND_MODULE = "mha-control-hub.js"
-
