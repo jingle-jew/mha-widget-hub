88f142cf4095afdb4abc972bc93944a26e6e837f
jingle-jew <jmb.whp@gmail.com>
2026-06-19 16:59:47 -0400

    Refactor settings panel to use shared shell contract

--- src/settings/settings-panel-coordinator.js
+++ src/settings/settings-panel-coordinator.js
@@ -5,21 +5,6 @@ import {
 } from "./settings-panel-props.js";
 import { appendLayoutModeControl } from "./layout-mode-control.js";
 import { replaceSettingsPanelPreservingUiState } from "./settings-panel-orchestrator.js";
-import {
-  applyPanelSurfaceContract,
-  PANEL_MOBILE_PRESENTATIONS,
-  PANEL_SURFACE_ROLES,
-} from "../panels/panel-surface-contract.js";
-
-function createPanelWithEnhancements(createPanel, props) {
-  return applyPanelSurfaceContract(
-    appendLayoutModeControl(createPanel(props)),
-    {
-      surfaceRole: PANEL_SURFACE_ROLES.PANEL,
-      mobilePresentation: PANEL_MOBILE_PRESENTATIONS.SHEET,
-    },
-  );
-}
 
 export function buildSettingsCoordinatorProps({
   settingsOpen = false,
@@ -88,14 +73,14 @@ export function syncSettingsPanels({
   const all = replaceSettingsPanelPreservingUiState({
     root,
     existing: root?.querySelector?.('.mha-settings-panel[data-settings-scope="all"]'),
-    next: createPanelWithEnhancements(createPanel, props.all),
+    next: appendLayoutModeControl(createPanel(props.all)),
     updatePanel,
   });
 
   const screensaver = replaceSettingsPanelPreservingUiState({
     root,
     existing: root?.querySelector?.('.mha-settings-panel[data-settings-scope="screensaver"]'),
-    next: createPanelWithEnhancements(createPanel, props.screensaver),
+    next: appendLayoutModeControl(createPanel(props.screensaver)),
     updatePanel,
   });
 
