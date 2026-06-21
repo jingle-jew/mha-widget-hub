4d573c5d93f7fe5cd9b910336b775d2e603adaa3
Jingle Jew <158747853+jingle-jew@users.noreply.github.com>
2026-06-20 21:43:29 -0400

    Load slider widget internals contract in frontend manifest

--- custom_components/mha_widget_hub/frontend/src/styles/style-manifest.js
+++ custom_components/mha_widget_hub/frontend/src/styles/style-manifest.js
@@ -60,6 +60,7 @@ const STATIC_STYLE_MANIFEST_AFTER_THEMES = Object.freeze([
 
 const STATIC_STYLE_MANIFEST_AFTER_WIDGETS = Object.freeze([
   entry("styles/widgets/widget-control-internals-contract.css", "component"),
+  entry("styles/widgets/widget-slider-internals-contract.css", "component"),
   entry("styles/screensaver/screensaver.css", "component"),
   entry("styles/screensaver/screensaver-contract.css", "component"),
   entry("styles/screensaver/screensaver-clock.css", "component"),
