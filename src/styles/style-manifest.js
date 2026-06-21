04f360a7479dac8279364afd0e2757d71869d69d
Jingle Jew <158747853+jingle-jew@users.noreply.github.com>
2026-06-20 21:16:32 -0400

    Load screensaver surface contract

--- src/styles/style-manifest.js
+++ src/styles/style-manifest.js
@@ -59,6 +59,7 @@ const STATIC_STYLE_MANIFEST_AFTER_THEMES = Object.freeze([
 
 const STATIC_STYLE_MANIFEST_AFTER_WIDGETS = Object.freeze([
   entry("styles/screensaver/screensaver.css", "component"),
+  entry("styles/screensaver/screensaver-contract.css", "component"),
   entry("styles/screensaver/screensaver-clock.css", "component"),
   entry("styles/screensaver/screensaver-hotcorner.css", "component"),
 ]);
